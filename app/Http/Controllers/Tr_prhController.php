e8ffa042f4943aab2f661918d8f3ce4a923be2ac
viery <vierybiliam8@gmail.com>
2025-09-09 13:57:16 +0700

    Change email to email Bos

--- app/Http/Controllers/Tr_prhController.php
+++ app/Http/Controllers/Tr_prhController.php
@@ -369,7 +369,7 @@ class Tr_prhController extends Controller
         $productName = $dt->pluck('fprdcode')->implode(', ');
         $approver = auth('sysuser')->user()->fname;
 
-        Mail::to('vierybiliam8@gmail.com')->send(new ApprovalEmail($hdr, $dt, $productName, $approver));
+        Mail::to('Surianto_w@yahoo.com')->send(new ApprovalEmail($hdr, $dt, $productName, $approver));
       }
     });
 
