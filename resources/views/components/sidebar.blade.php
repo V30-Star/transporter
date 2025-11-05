f6f9556af51f81225c8a0da6e27be24bcf0e33b9
viery <vierybiliam8@gmail.com>
2025-11-05 13:23:25 +0700

    FIxing sidebar

--- resources/views/components/sidebar.blade.php
+++ resources/views/components/sidebar.blade.php
@@ -57,14 +57,14 @@
                 </a>
             </li>
 
-            <!-- Master (accordion) -->
+            <!-- Master Accounting -->
             <li x-data="{ open: false }" x-effect="if(!openSidebar) open = false">
                 <button @click="open = !open"
                     class="flex items-center w-full p-2 rounded-lg hover:bg-gray-700 focus:outline-none">
                     <x-heroicon-o-folder class="w-5 h-5 flex-shrink-0" />
-                    <span class="ml-3 flex-1 text-left" x-show="openSidebar"
-                        x-transition.opacity.duration.150>Master</span>
-                    <!-- caret -->
+                    <span class="ml-3 flex-1 text-left" x-show="openSidebar" x-transition.opacity.duration.150>
+                        Master Accounting
+                    </span>
                     <svg x-show="openSidebar" :class="{ 'rotate-180': open }"
                         class="w-4 h-4 transition-transform ml-auto" fill="none" stroke="currentColor"
                         viewBox="0 0 24 24">
@@ -72,126 +72,15 @@
                     </svg>
                 </button>
 
-                <!-- Submenu -->
                 <ul x-show="open && openSidebar" x-transition class="ml-9 mt-1 space-y-1 border-l border-white/10 pl-3"
                     x-cloak>
 
-                    @if (in_array('viewCustomer', explode(',', session('user_restricted_permissions', ''))))
+                    @if (in_array('viewAccount', explode(',', session('user_restricted_permissions', ''))))
                         <li>
-                            <a href="{{ route('customer.index') }}"
+                            <a href="{{ route('account.index') }}"
                                 class="flex items-center p-2 rounded hover:bg-gray-700">
-                                <x-heroicon-o-user-group class="w-5 h-5" />
-                                <span class="ml-3">Customer</span>
-                            </a>
-                        </li>
-                    @endif
-
-                    @if (in_array('viewGroupCustomer', explode(',', session('user_restricted_permissions', ''))))
-                        <li>
-                            <a href="{{ route('groupcustomer.index') }}"
-                                class="flex items-center p-2 rounded hover:bg-gray-700">
-                                <x-heroicon-o-squares-2x2 class="w-5 h-5" />
-                                <span class="ml-3">Group Customer</span>
-                            </a>
-                        </li>
-                    @endif
-
-                    @if (in_array('viewWilayah', explode(',', session('user_restricted_permissions', ''))))
-                        <li>
-                            <a href="{{ route('wilayah.index') }}"
-                                class="flex items-center p-2 rounded hover:bg-gray-700">
-                                <x-heroicon-o-globe-alt class="w-5 h-5" />
-                                <span class="ml-3">Wilayah</span>
-                            </a>
-                        </li>
-                    @endif
-
-                    @if (in_array('viewSysuser', explode(',', session('user_restricted_permissions', ''))))
-                        <li>
-                            <a href="{{ route('sysuser.index') }}"
-                                class="flex items-center p-2 rounded hover:bg-gray-700">
-                                <x-heroicon-o-user-circle class="w-5 h-5" />
-                                <span class="ml-3">Wewenang User</span>
-                            </a>
-                        </li>
-                    @endif
-
-                    @if (in_array('viewSalesman', explode(',', session('user_restricted_permissions', ''))))
-                        <li>
-                            <a href="{{ route('salesman.index') }}"
-                                class="flex items-center p-2 rounded hover:bg-gray-700">
-                                <x-heroicon-o-briefcase class="w-5 h-5" />
-                                <span class="ml-3">Salesman</span>
-                            </a>
-                        </li>
-                    @endif
-
-                    @if (in_array('viewSatuan', explode(',', session('user_restricted_permissions', ''))))
-                        <li>
-                            <a href="{{ route('satuan.index') }}"
-                                class="flex items-center p-2 rounded hover:bg-gray-700">
-                                <x-heroicon-o-scale class="w-5 h-5" />
-                                <span class="ml-3">Satuan</span>
-                            </a>
-                        </li>
-                    @endif
-
-                    @if (in_array('viewMerek', explode(',', session('user_restricted_permissions', ''))))
-                        <li>
-                            <a href="{{ route('merek.index') }}"
-                                class="flex items-center p-2 rounded hover:bg-gray-700">
-                                <x-heroicon-o-tag class="w-5 h-5" />
-                                <span class="ml-3">Merek</span>
-                            </a>
-                        </li>
-                    @endif
-
-                    @if (in_array('viewGudang', explode(',', session('user_restricted_permissions', ''))))
-                        <li>
-                            <a href="{{ route('gudang.index') }}"
-                                class="flex items-center p-2 rounded hover:bg-gray-700">
-                                <x-heroicon-o-archive-box class="w-5 h-5" />
-                                <span class="ml-3">Gudang</span>
-                            </a>
-                        </li>
-                    @endif
-
-                    @if (in_array('viewGroupProduct', explode(',', session('user_restricted_permissions', ''))))
-                        <li>
-                            <a href="{{ route('groupproduct.index') }}"
-                                class="flex items-center p-2 rounded hover:bg-gray-700">
-                                <x-heroicon-o-squares-2x2 class="w-5 h-5" />
-                                <span class="ml-3">Group Produk</span>
-                            </a>
-                        </li>
-                    @endif
-
-                    @if (in_array('viewGroupProduct', explode(',', session('user_restricted_permissions', ''))))
-                        <li>
-                            <a href="{{ route('product.index') }}"
-                                class="flex items-center p-2 rounded hover:bg-gray-700">
-                                <x-heroicon-o-cube class="w-5 h-5" />
-                                <span class="ml-3">Product</span>
-                            </a>
-                        </li>
-                    @endif
-
-                    @if (in_array('viewSupplier', explode(',', session('user_restricted_permissions', ''))))
-                        <li>
-                            <a href="{{ route('supplier.index') }}"
-                                class="flex items-center p-2 rounded hover:bg-gray-700">
-                                <x-heroicon-o-truck class="w-5 h-5" />
-                                <span class="ml-3">Supplier</span>
-                            </a>
-                        </li>
-                    @endif
-
-                    @if (in_array('viewRekening', explode(',', session('user_restricted_permissions', ''))))
-                        <li>
-                            <a href="{{ route('rekening.index') }}"
-                                class="flex items-center p-2 rounded hover:bg-gray-700">
-                                <x-heroicon-o-credit-card class="w-5 h-5" />
-                                <span class="ml-3">Rekening</span>
+                                <x-heroicon-o-banknotes class="w-5 h-5" />
+                                <span class="ml-3">Account</span>
                             </a>
                         </li>
                     @endif
@@ -206,15 +95,179 @@
                         </li>
                     @endif
 
-                    @if (in_array('viewAccount', explode(',', session('user_restricted_permissions', ''))))
+                    @if (in_array('viewRekening', explode(',', session('user_restricted_permissions', ''))))
                         <li>
-                            <a href="{{ route('account.index') }}"
+                            <a href="{{ route('rekening.index') }}"
                                 class="flex items-center p-2 rounded hover:bg-gray-700">
-                                <x-heroicon-o-banknotes class="w-5 h-5" />
-                                <span class="ml-3">Account</span>
+                                <x-heroicon-o-credit-card class="w-5 h-5" />
+                                <span class="ml-3">Rekening</span>
                             </a>
                         </li>
                     @endif
+
+                </ul>
+            </li>
+
+            <!-- Master Barang -->
+            <li x-data="{ open: false }" x-effect="if(!openSidebar) open = false">
+                <button @click="open = !open"
+                    class="flex items-center w-full p-2 rounded-lg hover:bg-gray-700 focus:outline-none">
+                    <x-heroicon-o-folder class="w-5 h-5 flex-shrink-0" />
+                    <span class="ml-3 flex-1 text-left" x-show="openSidebar" x-transition.opacity.duration.150>
+                        Master Barang
+                    </span>
+                    <svg x-show="openSidebar" :class="{ 'rotate-180': open }"
+                        class="w-4 h-4 transition-transform ml-auto" fill="none" stroke="currentColor"
+                        viewBox="0 0 24 24">
+                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M19 9l-7 7-7-7" />
+                    </svg>
+                </button>
+
+                <ul x-show="open && openSidebar" x-transition class="ml-9 mt-1 space-y-1 border-l border-white/10 pl-3"
+                    x-cloak>
+
+                    @if (in_array('viewGroupProduct', explode(',', session('user_restricted_permissions', ''))))
+                        <li>
+                            <a href="{{ route('product.index') }}"
+                                class="flex items-center p-2 rounded hover:bg-gray-700">
+                                <x-heroicon-o-cube class="w-5 h-5" />
+                                <span class="ml-3">Product</span>
+                            </a>
+                        </li>
+                    @endif
+
+                    @if (in_array('viewGroupProduct', explode(',', session('user_restricted_permissions', ''))))
+                        <li>
+                            <a href="{{ route('groupproduct.index') }}"
+                                class="flex items-center p-2 rounded hover:bg-gray-700">
+                                <x-heroicon-o-squares-2x2 class="w-5 h-5" />
+                                <span class="ml-3">Group Produk</span>
+                            </a>
+                        </li>
+                    @endif
+
+                    @if (in_array('viewMerek', explode(',', session('user_restricted_permissions', ''))))
+                        <li>
+                            <a href="{{ route('merek.index') }}"
+                                class="flex items-center p-2 rounded hover:bg-gray-700">
+                                <x-heroicon-o-tag class="w-5 h-5" />
+                                <span class="ml-3">Merek</span>
+                            </a>
+                        </li>
+                    @endif
+
+                    @if (in_array('viewSatuan', explode(',', session('user_restricted_permissions', ''))))
+                        <li>
+                            <a href="{{ route('satuan.index') }}"
+                                class="flex items-center p-2 rounded hover:bg-gray-700">
+                                <x-heroicon-o-scale class="w-5 h-5" />
+                                <span class="ml-3">Satuan</span>
+                            </a>
+                        </li>
+                    @endif
+
+                </ul>
+            </li>
+
+            <!-- Master Penjualan -->
+            <li x-data="{ open: false }" x-effect="if(!openSidebar) open = false">
+                <button @click="open = !open"
+                    class="flex items-center w-full p-2 rounded-lg hover:bg-gray-700 focus:outline-none">
+                    <x-heroicon-o-folder class="w-5 h-5 flex-shrink-0" />
+                    <span class="ml-3 flex-1 text-left" x-show="openSidebar" x-transition.opacity.duration.150>
+                        Master Penjualan
+                    </span>
+                    <svg x-show="openSidebar" :class="{ 'rotate-180': open }"
+                        class="w-4 h-4 transition-transform ml-auto" fill="none" stroke="currentColor"
+                        viewBox="0 0 24 24">
+                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M19 9l-7 7-7-7" />
+                    </svg>
+                </button>
+
+                <ul x-show="open && openSidebar" x-transition class="ml-9 mt-1 space-y-1 border-l border-white/10 pl-3"
+                    x-cloak>
+
+                    @if (in_array('viewGroupCustomer', explode(',', session('user_restricted_permissions', ''))))
+                        <li>
+                            <a href="{{ route('groupcustomer.index') }}"
+                                class="flex items-center p-2 rounded hover:bg-gray-700">
+                                <x-heroicon-o-squares-2x2 class="w-5 h-5" />
+                                <span class="ml-3">Group Customer</span>
+                            </a>
+                        </li>
+                    @endif
+
+                    @if (in_array('viewCustomer', explode(',', session('user_restricted_permissions', ''))))
+                        <li>
+                            <a href="{{ route('customer.index') }}"
+                                class="flex items-center p-2 rounded hover:bg-gray-700">
+                                <x-heroicon-o-user-group class="w-5 h-5" />
+                                <span class="ml-3">Customer</span>
+                            </a>
+                        </li>
+                    @endif
+
+                    @if (in_array('viewWilayah', explode(',', session('user_restricted_permissions', ''))))
+                        <li>
+                            <a href="{{ route('wilayah.index') }}"
+                                class="flex items-center p-2 rounded hover:bg-gray-700">
+                                <x-heroicon-o-globe-alt class="w-5 h-5" />
+                                <span class="ml-3">Wilayah</span>
+                            </a>
+                        </li>
+                    @endif
+
+                    @if (in_array('viewSalesman', explode(',', session('user_restricted_permissions', ''))))
+                        <li>
+                            <a href="{{ route('salesman.index') }}"
+                                class="flex items-center p-2 rounded hover:bg-gray-700">
+                                <x-heroicon-o-briefcase class="w-5 h-5" />
+                                <span class="ml-3">Salesman</span>
+                            </a>
+                        </li>
+                    @endif
+
+                </ul>
+            </li>
+
+            <!-- Master Pembelian -->
+            <li x-data="{ open: false }" x-effect="if(!openSidebar) open = false">
+                <button @click="open = !open"
+                    class="flex items-center w-full p-2 rounded-lg hover:bg-gray-700 focus:outline-none">
+                    <x-heroicon-o-folder class="w-5 h-5 flex-shrink-0" />
+                    <span class="ml-3 flex-1 text-left" x-show="openSidebar" x-transition.opacity.duration.150>
+                        Master Pembelian
+                    </span>
+                    <svg x-show="openSidebar" :class="{ 'rotate-180': open }"
+                        class="w-4 h-4 transition-transform ml-auto" fill="none" stroke="currentColor"
+                        viewBox="0 0 24 24">
+                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M19 9l-7 7-7-7" />
+                    </svg>
+                </button>
+
+                <ul x-show="open && openSidebar" x-transition
+                    class="ml-9 mt-1 space-y-1 border-l border-white/10 pl-3" x-cloak>
+
+                    @if (in_array('viewGudang', explode(',', session('user_restricted_permissions', ''))))
+                        <li>
+                            <a href="{{ route('gudang.index') }}"
+                                class="flex items-center p-2 rounded hover:bg-gray-700">
+                                <x-heroicon-o-archive-box class="w-5 h-5" />
+                                <span class="ml-3">Gudang</span>
+                            </a>
+                        </li>
+                    @endif
+
+                    @if (in_array('viewSupplier', explode(',', session('user_restricted_permissions', ''))))
+                        <li>
+                            <a href="{{ route('supplier.index') }}"
+                                class="flex items-center p-2 rounded hover:bg-gray-700">
+                                <x-heroicon-o-truck class="w-5 h-5" />
+                                <span class="ml-3">Supplier</span>
+                            </a>
+                        </li>
+                    @endif
+
                 </ul>
             </li>
 
@@ -265,6 +318,37 @@
                             <span class="ml-3">Faktur Pembelian</span>
                         </a>
                     </li>
+                </ul>
+            </li>
+
+            <!-- Utility -->
+            <li x-data="{ open: false }" x-effect="if(!openSidebar) open = false">
+                <button @click="open = !open"
+                    class="flex items-center w-full p-2 rounded-lg hover:bg-gray-700 focus:outline-none">
+                    <x-heroicon-o-folder class="w-5 h-5 flex-shrink-0" />
+                    <span class="ml-3 flex-1 text-left" x-show="openSidebar"
+                        x-transition.opacity.duration.150>Utility</span>
+                    <!-- caret -->
+                    <svg x-show="openSidebar" :class="{ 'rotate-180': open }"
+                        class="w-4 h-4 transition-transform ml-auto" fill="none" stroke="currentColor"
+                        viewBox="0 0 24 24">
+                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M19 9l-7 7-7-7" />
+                    </svg>
+                </button>
+
+                <!-- Submenu -->
+                <ul x-show="open && openSidebar" x-transition
+                    class="ml-9 mt-1 space-y-1 border-l border-white/10 pl-3" x-cloak>
+
+                    @if (in_array('viewSysuser', explode(',', session('user_restricted_permissions', ''))))
+                        <li>
+                            <a href="{{ route('sysuser.index') }}"
+                                class="flex items-center p-2 rounded hover:bg-gray-700">
+                                <x-heroicon-o-user-circle class="w-5 h-5" />
+                                <span class="ml-3">Wewenang User</span>
+                            </a>
+                        </li>
+                    @endif
                 </ul>
             </li>
 
