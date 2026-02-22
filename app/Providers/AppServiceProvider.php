6d99a462406a1f32a3bae68468f481b4d551f612
V30-Star <vierybiliam8@gmail.com>
2026-02-22 15:29:53 +0700

    bug fixing

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -23,5 +23,10 @@ public function boot()
     Validator::extend('fsysuserid', function ($attribute, $value, $parameters, $validator) {
         return preg_match('/^[a-zA-Z0-9_]+$/', $value);
     });
+
+    // TAMBAHKAN KODE INI
+    if (!empty(env('APP_URL'))) {
+        \URL::forceRootUrl(env('APP_URL'));
+    }
 }
 }
