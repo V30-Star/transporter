fe6d25a321338460ad57c0095a2d404dcad0d777
IK190071\IK190071 <vierybiliam8@gmail.com>
2026-04-15 16:38:39 +0700

    refactor: Remove delete button from edit views for cleaner UI

--- resources/views/master/groupcustomer/edit.blade.php
+++ resources/views/master/groupcustomer/edit.blade.php
@@ -31,11 +31,6 @@
             </div>
 
             <div class="mt-6 flex justify-center space-x-4">
-                <button type="button" onclick="showDeleteModal()"
-                    class="bg-red-600 text-white px-6 py-2 rounded hover:bg-red-700 flex items-center">
-                    <x-heroicon-o-trash class="w-5 h-5 mr-2" />
-                    Hapus
-                </button>
                 <button type="button" onclick="window.location.href='{{ route('groupcustomer.index') }}'"
                     class="bg-gray-500 text-white px-6 py-2 rounded hover:bg-gray-600 flex items-center">
                     <x-heroicon-o-arrow-left class="w-5 h-5 mr-2" />
