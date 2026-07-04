433f0c5ecb22f8d7bdfafe9ae41b9dd0c3aaf07c
V30-Star <vierybiliam8@gmail.com>
2026-07-04 22:19:06 +0700

    fx

--- resources/views/product/edit.blade.php
+++ resources/views/product/edit.blade.php
@@ -929,9 +929,6 @@
                     @if ($isUsedProduct)
                         <div class="mb-4 rounded-lg border border-amber-200 bg-amber-50 px-4 py-3 text-sm text-amber-800">
                             PRODUK INI SUDAH DIREFERENSI DI TRANSAKSI {{ implode(', ', $usedByLabels) }}.
-                            KODE PRODUK, QTY KONVERSI, DAN SATUAN YANG SUDAH TERPAKAI DIKUNCI.
-                            ANDA MASIH BISA UBAH FIELD LAIN.
-                            Satuan 2 dan Satuan 3 masih boleh diisi atau diupdate jika slotnya masih kosong.
                         </div>
                     @endif
 
