5e6f74064fc931bf45a9d21ceeea2a6a43b3d8ff
V30-Star <vierybiliam8@gmail.com>
2026-07-25 13:24:50 +0700

    change format no transaction on surat jalan with ppn

--- app/Http/Controllers/SuratJalanController.php
+++ app/Http/Controllers/SuratJalanController.php
@@ -76,9 +76,11 @@ class SuratJalanController extends Controller
             return '-';
         }
 
-        $separator = $useSlash ? '/' : '.';
+        if ($useSlash) {
+            return str_replace('.', '/', $normalized);
+        }
 
-        return (string) preg_replace('/[.\/](\d+)$/', $separator . '$1', $normalized, 1);
+        return str_replace('/', '.', $normalized);
     }
 
     private function ensureNoDuplicateDetailCodes(array $codes, array $refs = [], array $qtys = []): void
@@ -957,15 +959,40 @@ class SuratJalanController extends Controller
 
                 // ---- 7.2. Generate nomor transaksi ----
                 if (empty($fstockmtno)) {
-                    $prefix = sprintf('%s.%s.%s%s.', $fstockmtcode, $kodeCabang, $yy, $mm);
-                    $lockKey = crc32('STOCKMT|' . $fstockmtcode . '|' . $kodeCabang . '|' . $fstockmtdate->format('y-m'));
-                    DB::statement('SELECT pg_advisory_xact_lock(?)', [$lockKey]);
+                    $hasPpn = (int) $fapplyppn === 1 || (int) $fincludeppn === 1;
+                    $sep = $hasPpn ? '.' : '/';
+                    $prefix = sprintf('%s%s%s%s%s%s', $fstockmtcode, $sep, $kodeCabang, $sep, $yy . $mm, $sep);
 
-                    $last = DB::table('trstockmt')
-                        ->where('fstockmtno', 'like', $prefix . '%')
-                        ->selectRaw("MAX(CAST(split_part(fstockmtno, '.', 4) AS int)) AS lastno")
-                        ->value('lastno');
-                    $next = (int) $last + 1;
+                    $lockKey = crc32('STOCKMT|' . $fstockmtcode . '|' . $kodeCabang . '|' . $fstockmtdate->format('y-m'));
+                    if (DB::getDriverName() === 'pgsql') {
+                        DB::statement('SELECT pg_advisory_xact_lock(?)', [$lockKey]);
+
+                        $last = DB::table('trstockmt')
+                            ->where(function ($q) use ($fstockmtcode, $kodeCabang, $yy, $mm) {
+                                $yymm = $yy . $mm;
+                                $q->where('fstockmtno', 'like', "{$fstockmtcode}.{$kodeCabang}.{$yymm}.%")
+                                  ->orWhere('fstockmtno', 'like', "{$fstockmtcode}/{$kodeCabang}/{$yymm}/%");
+                            })
+                            ->selectRaw("MAX(CAST(SUBSTRING(fstockmtno FROM '([0-9]+)$') AS int)) AS lastno")
+                            ->value('lastno');
+
+                        $next = (int) $last + 1;
+                    } else {
+                        $yymm = $yy . $mm;
+                        $lastCode = DB::table('trstockmt')
+                            ->where(function ($q) use ($fstockmtcode, $kodeCabang, $yymm) {
+                                $q->where('fstockmtno', 'like', "{$fstockmtcode}.{$kodeCabang}.{$yymm}.%")
+                                  ->orWhere('fstockmtno', 'like', "{$fstockmtcode}/{$kodeCabang}/{$yymm}/%");
+                            })
+                            ->orderByDesc('fstockmtno')
+                            ->value('fstockmtno');
+
+                        $next = 1;
+                        if ($lastCode && ($pos = max((int) strrpos($lastCode, '.'), (int) strrpos($lastCode, '/'))) !== false && $pos > 0) {
+                            $next = ((int) substr($lastCode, $pos + 1)) + 1;
+                        }
+                    }
+
                     $fstockmtno = $prefix . str_pad((string) $next, 4, '0', STR_PAD_LEFT);
 
                     Log::debug("SuratJalan@store: Nomor SRJ di-generate otomatis [{$fstockmtno}].");
