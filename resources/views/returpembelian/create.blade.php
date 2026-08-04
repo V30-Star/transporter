742b7a54a602bef7f0aaee69e3113938f62d9ecb
V30-Star <vierybiliam8@gmail.com>
2026-08-04 20:07:10 +0700

    fx

--- resources/views/returpembelian/create.blade.php
+++ resources/views/returpembelian/create.blade.php
@@ -674,8 +674,6 @@
                                                         <input type="hidden" name="fketdt[]" :value="dr.fketdt || ''">
                                                         <input type="hidden" name="frefdtno[]" :value="dr.frefdtno || ''">
                                                     </td>
-                                                </template>
-                                            </tr>
                                         </template>
                                     </tbody>
                                 </table>
