eef2e9c421c71cbcee546437183c6e551c3ebcc6
IK190071\IK190071 <vierybiliam8@gmail.com>
2026-08-06 09:17:41 +0700

    add title is listing so

--- resources/views/listingso/index.blade.php
+++ resources/views/listingso/index.blade.php
@@ -1,6 +1,6 @@
 @extends('layouts.app')
 
-@section('title', '')
+@section('title', 'Listing SO')
 
 @section('content')
     <div id="filterModal" class="fixed inset-0 z-50 hidden overflow-y-auto">
