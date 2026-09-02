aed09f2a5f16bc965fb1874009d431748acee5e4
IK190071\IK190071 <vierybiliam8@gmail.com>
2026-09-02 14:12:15 +0700

    fx

--- resources/views/currency/create.blade.php
+++ resources/views/currency/create.blade.php
@@ -69,7 +69,7 @@
             <div class="bg-white border border-gray-200 rounded-xl mb-3 overflow-hidden">
                 <div class="flex items-center gap-2 px-4 pt-3 pb-0">
                     <svg xmlns="http://www.w3.org/2000/svg" class="w-4 h-4 text-gray-400" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 19v-6a2 2 0 00-2-2H5a2 2 0 00-2 2v6a2 2 0 002 2h2a2 2 0 002-2zm0 0V9a2 2 0 012-2h2a2 2 0 012 2v10m-6 0a2 2 0 002 2h2a2 2 0 002-2m0 0V5a2 2 0 012-2h2a2 2 0 012 2v14a2 2 0 01-2 ２h-２a２ ２ ０ ０１-２-２z" />
+                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 19v-6a2 2 0 00-2-2H5a2 2 0 00-2 2v6a2 2 0 002 2h2a2 2 0 002-2zm0 0V9a2 2 0 012-2h2a2 2 0 012 2v10m-6 0a2 2 0 002 2h2a2 2 0 002-2m0 0V5a2 2 0 012-2h2a2 2 0 012 2v14a2 2 0 01-2 2h-2a2 2 0 01-2-2z" />
                     </svg>
                     <p class="text-xs font-bold uppercase tracking-wide text-gray-400">Status</p>
                 </div>
